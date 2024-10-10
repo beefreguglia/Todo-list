160262fa0eadd6e5f7bf513c7b2085ae67de3c2a
Bernardo <beefreguglia@gmail.com>
2024-10-09 23:23:23 -0300

    feat: create env package

---
+++ packages/env/index.ts
@@ -0,0 +1,17 @@
+import { createEnv } from '@t3-oss/env-nextjs'
+import { z } from 'zod'
+
+export const env = createEnv({
+  server: {
+    PORT: z.coerce.number().default(3333),
+
+    DATABASE_URL: z.string().url(),
+  },
+  client: {},
+  shared: {},
+  runtimeEnv: {
+    PORT: process.env.PORT,
+    DATABASE_URL: process.env.DATABASE_URL,
+  },
+  emptyStringAsUndefined: true,
+})
