0cda2e1d592bc52ea778df7a487b82992e2d3391
Bernardo <beefreguglia@gmail.com>
2024-10-10 15:15:05 -0300

    feat: create user decorator

--- apps/api/src/controllers/create-task.controller.ts
+++ apps/api/src/controllers/create-task.controller.ts
@@ -1,4 +1,6 @@
 import { Controller, Post, UseGuards } from '@nestjs/common'
+import { CurrentUser } from 'src/auth/current-user-decorator'
+import { UserPayload } from 'src/auth/jwt.strategy'
 import { JwtAuthGuard } from 'src/auth/jwt-auth.guard'
 
 @Controller('/tasks')
@@ -7,7 +9,8 @@ export class CreateTaskController {
   constructor() {}
 
   @Post()
-  async handle() {
+  async handle(@CurrentUser() user: UserPayload) {
+    console.log(user)
     return 'ok'
   }
 }
