ad24c334b542188857264ce7a01a04f7008722f9
Bernardo <beefreguglia@gmail.com>
2024-10-11 20:02:11 -0300

    feature: allow localhost on api

--- apps/api/src/infra/main.ts
+++ apps/api/src/infra/main.ts
@@ -6,6 +6,13 @@ import { Env } from './env'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
+  app.enableCors({
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true,
+  })
+
   const configService = app.get<ConfigService<Env, true>>(ConfigService)
 
   const PORT = configService.get('PORT', { infer: true })
