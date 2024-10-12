c961e4bfc5270b99f9722881175e4ad321aef063
Bernardo <beefreguglia@gmail.com>
2024-10-12 11:38:35 -0300

    feature: create task component

--- apps/web/src/components/ui/task.tsx
+++ apps/web/src/components/ui/task.tsx
@@ -1,12 +1,42 @@
+import { Controller, useFormContext } from 'react-hook-form'
+
 import { Checkbox } from '@/components/ui/checkbox'
 
 import { OptionsButton } from './options-button'
 
-export function Task() {
+interface TaskProps {
+  task: {
+    title: string
+    content: string
+    createdAt: Date
+    finishedAt?: Date | null
+    checked: boolean
+    id: string
+    fieldIndex: number
+  }
+}
+
+export function Task({ task }: TaskProps) {
+  const { control } = useFormContext()
+
   return (
-    <div className="rounded-md bg-slate-950 p-3 text-slate-100">
+    <div className="w-[99%] rounded-md bg-slate-950 p-3 text-slate-100">
       <div className="flex items-center justify-between">
-        <Checkbox checked id="task1" label="task1" />
+        <Controller
+          name={`interval.${task.fieldIndex}.checked`}
+          control={control}
+          render={({ field }) => (
+            <Checkbox
+              id={task.id}
+              label={task.content}
+              checked={field.value}
+              onCheckedChange={(checked) => {
+                field.onChange(checked === true)
+              }}
+            />
+          )}
+        />
+
         <OptionsButton />
       </div>
     </div>
