9ca853eaf51398ba842b684a376c42bb3cf20a19
Bernardo <beefreguglia@gmail.com>
2024-10-13 03:04:34 -0300

    fix: styles in mobile

--- apps/web/src/components/layouts/auth-layout.tsx
+++ apps/web/src/components/layouts/auth-layout.tsx
@@ -22,7 +22,7 @@ export default function AuthLayout({
   }, [router])
 
   return (
-    <main className="grid min-h-screen grid-cols-1 gap-5 overflow-hidden bg-slate-950 p-5 lg:grid-cols-[1fr_30rem]">
+    <main className="grid h-screen grid-cols-1 gap-5 overflow-hidden bg-slate-950 p-5 lg:grid-cols-[1fr_30rem]">
       <div className="w-full rounded-md">{children}</div>
       <aside className="hidden flex-col rounded-md bg-slate-900 px-8 py-20 shadow-md shadow-slate-900 lg:flex">
         <h2 className="z-10 p-1 font-sans text-2xl font-medium text-slate-200">
--- apps/web/src/components/layouts/layout.tsx
+++ apps/web/src/components/layouts/layout.tsx
@@ -17,7 +17,7 @@ export default function Layout({ children }: AuthLayoutProps) {
   }, [router])
 
   return (
-    <div className="grid h-screen grid-cols-[20%_1fr] overflow-hidden bg-slate-900 px-2 py-4 lg:grid-cols-[20rem_1fr]">
+    <div className="grid h-full grid-cols-[20%_1fr] overflow-hidden bg-slate-900 px-2 py-4 lg:grid-cols-[20rem_1fr]">
       <Sidebar />
       <div>{children}</div>
     </div>
